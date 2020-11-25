4ebbd921a5eb4546ba80c5f6afe32c96e282df76
Aman Goyal <im.developer26@gmail.com>
2020-11-26 04:09:47 +0530

    login page created

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import classes from './Login.module.css';
+
+import loginImg from '../../assets/login.svg';
+import { Button, TextField } from '@material-ui/core';
+
+class Login extends Component {
+
+    state = {
+        userId: '',
+        password: ''
+    }
+
+    render() {
+
+        const { userId, password } = this.state;
+
+        return (
+            <div className={classes.login}>
+
+                <div className={classes.card}>
+
+                    <div className={classes.cardLeft}>
+                        <img className={classes.img} src={loginImg} alt="login"></img>
+                    </div>
+
+                    <div className={classes.cardRight}>
+
+                        <h1>Aggrawal Provision Store</h1>
+
+                        <form className={classes.form}>
+
+                            <TextField
+                                className={classes.formInput}
+                                variant="outlined"
+                                placeholder="User Id"
+                                value={userId}
+                                onChange={(e) => this.setState({ userId: e.target.value })}
+                            />
+
+                            <TextField
+                                className={classes.formInput}
+                                variant="outlined"
+                                placeholder="Password"
+                                value={password}
+                                onChange={(e) => this.setState({ password: e.target.value })}
+                            />
+                            <Button variant="contained" color="primary">Submit</Button>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default Login;
---
+++ src/theme.js
@@ -0,0 +1,20 @@
+const { createMuiTheme } = require("@material-ui/core")
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#b79ced',
+            main: '#826aed',
+            dark: '#7161ef',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#ffb7ff',
+            main: '#c879ff',
+            dark: '#c04cfd',
+            contrastText: '#000',
+        },
+    }
+})
+
+export default theme
